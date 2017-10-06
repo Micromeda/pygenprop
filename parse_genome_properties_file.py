1363a024b2f5e4fc3c24e35541ca62fcb3088354
Lee Bergstrand <leemacboy@gmail.com>
2017-10-06 15:02:59 -0400

    Clean up executing file.

--- parse_genome_properties_file.py
+++ parse_genome_properties_file.py
@@ -10,8 +10,8 @@ from modules.lib import parse_genome_property_file
 
 genome_property_flat_file_path = '../genome-properties/docs/release/genomeProperties.txt'
 
-with open(genome_property_flat_file_path) as gen_prop_file:
-    properties = parse_genome_property_file(gen_prop_file)
+with open(genome_property_flat_file_path) as genome_property_file:
+    properties = parse_genome_property_file(genome_property_file)
 
-# print(properties)
-
+for genome_property in properties:
+    print(genome_property)
