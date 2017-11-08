a2889a31f5de23c2e000a785596ffbb0cdfae92a
Lee Bergstrand <leemacboy@gmail.com>
2017-11-08 15:01:19 -0500

    modify step parsing taking to account presence of functional elements.

--- modules/step.py
+++ modules/step.py
@@ -6,49 +6,32 @@ Created by: Lee Bergstrand (2017)
 Description: The step class.
 """
 
+from modules.functional_element import parse_functional_element
+
 
 class Step(object):
     """A class representing a step that supports the existence of a genome property."""
 
-    def __init__(self, number, identifier, name, gene_ontology_ids=None, evidence=None, required=False,
-                 sufficient=False):
+    def __init__(self, number, functional_elements=None):
         """
         Creates a new Step object.
         :param number: The position of the step in the step list.
-        :param identifier: The identifier of the step.
-        :param name: The name of the step.
-        :param evidence: A list of identifiers of proteins or processes supporting this step
-                        (i.e a list of Intro Consortium IDs or GenProp Accessions).
-        :param gene_ontology_ids: The id for gene ontology (GO) term of the protein supporting this step.
-        :param required: Is this a required step for this genome property?
-        :param sufficient: IS this step sufficient? (TODO: figure out what this means?)
+        :param functional_elements: A list of FunctionalElements supporting this step.
         """
 
-        if evidence is None:
-            evidence = set
-        if gene_ontology_ids is None:
-            gene_ontology_ids = set
-        if required is None:
-            required = False
-        if sufficient is None:
-            sufficient = False
+        if functional_elements is None:
+            functional_elements = set
 
         self.number = int(number)
         self.id = identifier
         self.name = name
-        self.evidence = evidence
-        self.gene_ontology_ids = gene_ontology_ids
-        self.required = required
-        self.sufficient = sufficient
+        self.functional_elements = functional_elements
 
     def __repr__(self):
         repr_data = ['Step ' + str(self.number),
                      'ID: ' + str(self.id),
                      'Name: ' + str(self.name),
-                     'Evidences: ' + str(self.evidence),
-                     'Gene Ontology IDs: ' + str(self.gene_ontology_ids),
-                     'Required: ' + str(self.required),
-                     'Sufficient: ' + str(self.sufficient)]
+                     'Evidences: ' + str(self.functional_elements)]
         return ', '.join(repr_data)
 
 
@@ -60,45 +43,14 @@ def parse_steps(genome_property_record):
     """
     step_markers = ('SN', 'ID', 'DN', 'RQ', 'EV', 'TG')
     steps = []
-    current_step = {}
+    current_step_markers = []
     for marker, content in genome_property_record:
         if marker in step_markers:
-            if marker in current_step:
-                steps.append(Step(number=current_step.get('SN'), identifier=current_step.get('ID'),
-                                  name=current_step.get('DN'), evidence=current_step.get('EV'),
-                                  gene_ontology_ids=current_step.get('TG'), required=current_step.get('RQ'),
-                                  sufficient=current_step.get('SF')))
-
-                if marker == 'SN':
-                    content = int(content)
-
-                current_step = {marker: content}
+            if not marker == 'SN':
+                current_step_markers.append((marker, content))
             else:
-                if marker == 'SN':
-                    content = int(content)
-                elif marker == 'EV' or marker == 'TG':
-                    split_content = filter(None, content.split(';'))
-                    cleaned_content = set(map(lambda evidence: evidence.strip(), split_content))
-                    if marker == 'EV':
-                        if 'sufficient' in cleaned_content:
-                            current_step['SF'] = True
-                        else:
-                            current_step['SF'] = False
-
-                        content = set(evidence for evidence in cleaned_content if evidence != 'sufficient')
-                    else:
-                        content = cleaned_content
-                elif marker == 'RQ':
-                    if int(content) == 1:
-                        content = True
-                    else:
-                        content = False
-
-                current_step[marker] = content
-
-    steps.append(Step(number=current_step.get('SN'), identifier=current_step.get('ID'),
-                      name=current_step.get('DN'), evidence=current_step.get('EV'),
-                      gene_ontology_ids=current_step.get('TG'), required=current_step.get('RQ'),
-                      sufficient=current_step.get('SF')))
-
+                step_number = content
+                functional_elements = parse_functional_element(current_step_markers)
+                steps.append(Step(number=step_number, functional_elements=functional_elements))
+                current_step_markers = []
     return steps
