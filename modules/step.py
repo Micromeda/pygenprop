9014c6cfb49ac256a7759ef7bcf2fb9568df4da8
Lee Bergstrand <leemacboy@gmail.com>
2017-11-09 15:45:20 -0500

    Implement updates to the step class.

--- modules/step.py
+++ modules/step.py
@@ -6,7 +6,7 @@ Created by: Lee Bergstrand (2017)
 Description: The step class.
 """
 
-from modules.functional_element import parse_functional_element
+from modules.functional_element import parse_functional_elements
 
 
 class Step(object):
@@ -23,16 +23,27 @@ class Step(object):
             functional_elements = set
 
         self.number = int(number)
-        self.id = identifier
-        self.name = name
         self.functional_elements = functional_elements
 
     def __repr__(self):
         repr_data = ['Step ' + str(self.number),
-                     'ID: ' + str(self.id),
-                     'Name: ' + str(self.name),
                      'Evidences: ' + str(self.functional_elements)]
         return ', '.join(repr_data)
+
+    @property
+    def required(self):
+        """
+        Checks if the step is required by checking if any of the functional elements are required.
+        :return: True if the step is required.
+        """
+        required_step = False
+
+        for element in self.functional_elements:
+            if element.required:
+                required_step = True
+                break
+
+        return required_step
 
 
 def parse_steps(genome_property_record):
@@ -44,13 +55,20 @@ def parse_steps(genome_property_record):
     step_markers = ('SN', 'ID', 'DN', 'RQ', 'EV', 'TG')
     steps = []
     current_step_markers = []
+    step_number = 0
     for marker, content in genome_property_record:
         if marker in step_markers:
             if not marker == 'SN':
                 current_step_markers.append((marker, content))
             else:
-                step_number = content
-                functional_elements = parse_functional_element(current_step_markers)
-                steps.append(Step(number=step_number, functional_elements=functional_elements))
-                current_step_markers = []
+                if current_step_markers:
+                    functional_elements = parse_functional_elements(current_step_markers)
+                    steps.append(Step(number=step_number, functional_elements=functional_elements))
+                    current_step_markers = []
+                else:
+                    step_number = int(content)
+
+    functional_elements = parse_functional_elements(current_step_markers)
+    steps.append(Step(number=step_number, functional_elements=functional_elements))
+
     return steps
