314d996a19e63139df96564ad6b77672d83db8eb
Lee Bergstrand <leemacboy@gmail.com>
2018-09-21 21:21:18 -0400

    Added wrapper code for end to end tests.

--- test_parse_genome_properties_assignment.py
+++ test_parse_genome_properties_assignment.py
@@ -9,6 +9,8 @@ Description: Parses EBI genome properties assignment files.
 import argparse
 
 from modules.genome_properties_longform_file_parser import parse_genome_property_longform_file
+from modules.genome_properties_flat_file_parser import parse_genome_property_file
+from modules.genome_properties_results import GenomePropertiesResults
 
 from modules.lib import sanitize_cli_path
 
@@ -19,21 +21,34 @@ def main(args):
     :param args: The command line arguments.
     """
 
-    genome_property_assignment_file_path = sanitize_cli_path(args.input_genome_properties_file)
+    genome_property_flat_file_path = sanitize_cli_path(args.input_genome_properties_flat_file)
+    json_output_path = sanitize_cli_path(args.output_file_path)
+    assignment_file_paths = (sanitize_cli_path(path) for path in args.input_genome_properties_assignment_files)
 
-    with open(genome_property_assignment_file_path) as assignment_file:
-        properties_assignment = parse_genome_property_longform_file(assignment_file)
+    with open(genome_property_flat_file_path) as genome_property_file:
+        genome_properties_tree = parse_genome_property_file(genome_property_file)
 
-    if properties_assignment is not None:
-        print(properties_assignment)
+    assignments = []
+    for path in assignment_file_paths:
+        with open(path) as assignment_file:
+            assignments.append(parse_genome_property_longform_file(assignment_file))
+
+    results = GenomePropertiesResults(*assignments, genome_properties_tree=genome_properties_tree)
+
+    with open(json_output_path, 'w') as json_file:
+        results.to_json(json_file)
 
 
 if __name__ == '__main__':
     cli_title = """Parses a genome properties assignment file and prints its contents in a human readable format."""
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('-i', '--input_genome_properties_file', metavar='DB', required=True,
+    parser.add_argument('-d', '--input_genome_properties_flat_file', metavar='DB', required=True,
                         help='The path to the genome properties flat file.')
+    parser.add_argument('-o', '--output_file_path', metavar='OUT', required=True,
+                        help='The path to the genome properties flat file.')
+    parser.add_argument('-i', '--input_genome_properties_assignment_files', metavar='ASSIGN', nargs='+',
+                        required=True, help='The path to the genome properties flat file.')
 
     cli_args = parser.parse_args()
 
