a4a3a924d73e55ff880c7586f325e5061a3e7827
Lee Bergstrand <leemacboy@gmail.com>
2018-11-16 19:59:18 -0500

    Improve shell scripts.

--- testing/test_end_to_end.sh
+++ testing/test_end_to_end.sh
@@ -3,7 +3,8 @@
 NUMBER_OF_PROPS=$(grep 'AC  GenProp' testing/test_constants/test_genome_properties.txt | wc -l | tr -d ' ')
 NUMBER_OF_PROPS_PARSED=$(./test_parse_genome_properties_file.py -i testing/test_constants/test_genome_properties.txt | grep -o '^[1-9]* ')
 
-if [ ${NUMBER_OF_PROPS} == ${NUMBER_OF_PROPS_PARSED} ];then
+if [[ ${NUMBER_OF_PROPS} == ${NUMBER_OF_PROPS_PARSED} ]];
+then
   echo "The correct number of genome properties were parsed.";
   exit 0
 else
