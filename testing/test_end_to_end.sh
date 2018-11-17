1d8c9ffe3073b3cfbe3036683f009baaf50b651f
Lee Bergstrand <leemacboy@gmail.com>
2018-11-16 20:25:03 -0500

    Update end to end scripts and genome properties URL.

--- testing/test_end_to_end.sh
+++ testing/test_end_to_end.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
+set -euxo pipefail
 
 NUMBER_OF_PROPS=$(grep 'AC  GenProp' testing/test_constants/test_genome_properties.txt | wc -l | tr -d ' ')
 NUMBER_OF_PROPS_PARSED=$(./test_parse_genome_properties_file.py -i testing/test_constants/test_genome_properties.txt | grep -o '^[1-9]* ')
+
+echo "Number of GREPed genome properties: ${NUMBER_OF_PROPS}"
+echo "Number of parsed genome properties: ${NUMBER_OF_PROPS_PARSED}"
 
 if [[ ${NUMBER_OF_PROPS} == ${NUMBER_OF_PROPS_PARSED} ]];
 then
