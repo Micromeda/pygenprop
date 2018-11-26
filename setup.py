5ce21100b42dad13815d21cd3fae9a2710756314
Lee Bergstrand <leemacboy@gmail.com>
2018-11-25 21:16:53 -0500

    add setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+"""
+Created by: Lee Bergstrand (2018)
+
+Description: Setup for installing pygenprop.
+"""
+
+from setuptools import setup
+
+setup(name='pygenprop',
+      version='0.1',
+      description='A python library for programmatic usage of EBI InterPro Genome Properties.',
+      url='https://github.com/Micromeda/pygenprop',
+      author='Lee Bergstrand',
+      author_email='flyingcircus@example.com',
+      license='Apache License 2.0',
+      packages=['pygenprop'],
+      install_requires=[
+          'Cython==0.29',
+          'pandas==0.23.4',
+      ],
+      zip_safe=False)
