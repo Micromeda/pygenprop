31ac49e7c681a4339a3b26fa9a2ada5bf1b83222
Lee Bergstrand <leemacboy@gmail.com>
2018-11-25 21:33:35 -0500

    Travis-CI

--- test_end_to_end.sh
+++ test_end_to_end.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-NUMBER_OF_PROPS=$(grep 'AC  GenProp' testing/test_constants/test_genome_properties.txt | wc -l | tr -d ' ')
+NUMBER_OF_PROPS=$(grep 'AC  GenProp' ./pygenprop/testing/test_constants/test_genome_properties.txt | wc -l | tr -d ' ')
 NUMBER_OF_PROPS_PARSED=$(./test_parse_genome_properties_file.py  -i ./pygenprop/testing/test_constants/test_genome_properties.txt | grep -o '^[1-9]*')
 
 echo "Number of GREPed genome properties: ${NUMBER_OF_PROPS}"
