fadf282448bae4ad8fa59c3a904e65e11d8664ef
Lee Bergstrand <leemacboy@gmail.com>
2018-11-25 21:16:31 -0500

    Move end to end tests.

--- test_end_to_end.sh
+++ test_end_to_end.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 NUMBER_OF_PROPS=$(grep 'AC  GenProp' testing/test_constants/test_genome_properties.txt | wc -l | tr -d ' ')
-NUMBER_OF_PROPS_PARSED=$(./test_parse_genome_properties_file.py -i testing/test_constants/test_genome_properties.txt | grep -o '^[1-9]*')
+NUMBER_OF_PROPS_PARSED=$(test_parse_genome_properties_file.py -i testing/test_constants/test_genome_properties.txt | grep -o '^[1-9]*')
 
 echo "Number of GREPed genome properties: ${NUMBER_OF_PROPS}"
 echo "Number of parsed genome properties: ${NUMBER_OF_PROPS_PARSED}"
