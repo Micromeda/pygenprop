8ee376324bd38df939b39ec378ad66889869b7b0
Lee Bergstrand <leemacboy@gmail.com>
2019-01-22 17:58:20 -0500

    rename assignment file parser module to assignment parsers. Add second parser for parsing interproscan files.

---
+++ pygenprop/assignment_parsers.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+
+"""
+Created by: Lee Bergstrand (2017)
+
+Description: A parser for parsing genome properties longform files.
+"""
+import csv
+from os.path import basename, splitext
+from pygenprop.assign import AssignmentCache, create_assignment_cache_from_interpro_member_database_identifiers
+
+
+def parse_genome_property_longform_file(longform_file):
+    """
+    Parses longform genome properties assignment files.
+
+    :param longform_file: A longform genome properties assignment file handle object.
+    :return: An assignment cache object.
+    """
+    property_id = ''
+    step_number = ''
+
+    assignment_cache = AssignmentCache(sample_name=splitext(basename(longform_file.name))[0])
+
+    for line in longform_file:
+        if 'PROPERTY:' in line:
+            property_id = line.split(':')[1].strip()
+        elif 'STEP NUMBER:' in line:
+            step_number = int(line.split(':')[1].strip())
+        elif 'RESULT:' in line:
+            assignment = line.split(':')[1].strip().upper()
+
+            if 'STEP' in line:
+                assignment_cache.cache_step_assignment(property_id, step_number, assignment)
+            else:
+                assignment_cache.cache_property_assignment(property_id, assignment)
+        else:
+            continue
+
+    return assignment_cache
+
+
+def parse_interproscan_results_file(interproscan_file):
+    """
+
+    :param interproscan_file: A InterProScan file handle object.
+    :return: An assignment cache object.
+    """
+    identifiers = []
+    tsv_reader = csv.reader(interproscan_file, delimiter='\t')
+    for row in tsv_reader:
+        matched_interpro_member_database_id = row[4]
+        identifiers.append(matched_interpro_member_database_id)
+
+    return create_assignment_cache_from_interpro_member_database_identifiers(identifiers)
